b1380db811f47c87fdb7a33b68f3c42a7ee2cac2
Vieirauc <lpedro.vieira18@gmail.com>
2023-05-18 01:00:17 +0100

    add admin token

---
+++ postgresql/commands.sql
@@ -0,0 +1,3 @@
+INSERT INTO publisher (name)
+VALUES ('Think Music Record')
+RETURNING id;
--- python/music_api.py
+++ python/music_api.py
@@ -33,13 +33,13 @@ StatusCodes = {
 def hash_password(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
+
 def check_password(password, hash):
     hashed = hash_password(password)
     if hashed.strip() == hash.strip():
         return True
     else:
         return False
-
 
 #########################################################
 ## TOKEN REQUIRED
@@ -56,9 +56,10 @@ def token_required(func):
         if not token:
             return flask.jsonify({"alerta": "Missing Token!"}), 400
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             user_id = data['user_id']
         except:
+
             return flask.jsonify({'alerta': 'Invalid Token!'}), 400
         return func(user_id,*args, **kwargs)
     return decorated
@@ -107,8 +108,8 @@ def add_user():
 
     # do not forget to validate every argument, e.g.,:
     
-    if 'username' not in payload or 'email' not in payload or 'password_hash' not in payload:
-        response = {'status': StatusCodes['api_error'], 'results': 'username, email or password_hash value not in payload'}
+    if 'username' not in payload or 'email' not in payload or 'password' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'username, email or password value not in payload'}
         return flask.jsonify(response)
     
     if 'role' not in payload:
@@ -117,48 +118,87 @@ def add_user():
     
     # parameterized queries, good for security and performance
     if(payload['role'] == 'admin'):
-        statement = 'INSERT INTO account (username, email, password_hash) VALUES (%s, %s, %s) RETURNING id'
-        values = (payload['username'], payload['email'], payload['password_hash'])
+        statement = '''
+            INSERT INTO account (username, email, password_hash)
+            VALUES (%s, %s, %s)
+            RETURNING id
+        '''
+        values = (payload['username'], payload['email'], hash_password(payload['password']))
+        cur.execute(statement, values)
+        account_id = cur.fetchone()[0]
 
-        try:
-            cur.execute(statement, values)
-            account_id = cur.fetchone()[0]
-            statement = 'INSERT INTO administrator (account_id) VALUES (%s)'
-            values = (account_id,)
-            cur.execute(statement, values)
-            # commit the transaction
-            conn.commit()
-            response = {'status': StatusCodes['success'], 'results': f'Inserted account {payload["role"]} {payload["username"]}'}
-        
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(f'POST /dbproj/user - error: {error}')
-            response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-            # an error occurred, rollback
-            conn.rollback()
-        
-        finally:
-            if conn is not None:
-                conn.close()
+        statement = '''
+            INSERT INTO administrator (account_id)
+            VALUES (%s)
+        '''
+        values = (account_id,)
+        cur.execute(statement, values)
+        conn.commit()
+
+        response = {'status': StatusCodes['success'], 'results': f'User {payload["username"]} created'}
+        return flask.jsonify(response)
 
     elif(payload['role'] == 'regular'):
-        statement = 'INSERT INTO account (username, email, password_hash) VALUES (%s, %s, %s)'
-        values = (payload['username'], payload['email'], hash_password(payload['password_hash']))
+        statement = '''
+            INSERT INTO account (username, email, password_hash)
+            VALUES (%s, %s, %s)
+            RETURNING id
+        '''
+        values = (payload['username'], payload['email'], hash_password(payload['password']))
+        cur.execute(statement, values)
+        account_id = cur.fetchone()[0]
 
+        statement = '''
+            INSERT INTO regular (account_id)
+            VALUES (%s)
+        '''
+        values = (account_id,)
+        cur.execute(statement, values)
+        conn.commit()
+
+        response = {'status': StatusCodes['success'], 'results': f'User {payload["username"]} created'}
+        return flask.jsonify(response)
+        
+    #Add artist and resquest admin token in header
+    elif(payload['role'] == 'artist'):
+        if 'admin-token' in flask.request.headers:
+            token = flask.request.headers['admin-token']
+
+        if not token:
+            return flask.jsonify({"alerta": "Missing Admin Token!"}), 400
+        
         try:
-            cur.execute(statement, values)
-            # commit the transaction
-            conn.commit()
-            response = {'status': StatusCodes['success'], 'results': f'Inserted account {payload["role"]} {payload["username"]}'}
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            user_id = data['user_id']
+            role = data['role']
+            if(role != 'admin'):
+                return flask.jsonify({"alerta": "Need admin permission!"}), 400
+        except:
+            return flask.jsonify({'alerta': 'Invalid Token!'}), 400
+        
+        statement = '''
+            INSERT INTO account (username, email, password_hash)
+            VALUES (%s, %s, %s)
+            RETURNING id
+        '''
+        values = (payload['username'], payload['email'], hash_password(payload['password']))
+        cur.execute(statement, values)
+        account_id = cur.fetchone()[0]
 
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(f'POST /dbproj/user - error: {error}')
-            response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-            # an error occurred, rollback
-            conn.rollback()
+        statement = '''
+            WITH publisher_id_cte AS (
+            SELECT id
+            FROM publisher
+            WHERE name = %s
+            )
+            INSERT INTO artist (account_id, artistic_name, publisher_id)
+            VALUES (%s, %s, (SELECT id FROM publisher_id_cte));
+        '''
+        values = (payload['publisher'],account_id,payload['artistic_name'])
+        cur.execute(statement, values)
+        conn.commit()
 
-        finally:
-            if conn is not None:
-                conn.close()
+        response = {'status': StatusCodes['success'], 'results': f'User {payload["username"]} created'}
 
     return flask.jsonify(response)
 
@@ -173,37 +213,100 @@ def login_user():
     
     logger.debug(f'PUT /dbproj/user - payload: {payload}')
 
-    try:
-        # parameterized queries, good for security and performance
-        cur.execute('SELECT username,password_hash,id FROM account WHERE username = %s',(payload['username'],))
-        rows = cur.fetchall()
+    # Verificar se todos os campos obrigatórios estão presentes no payload
+    required_fields = ['username', 'password']
+    missing_fields = [field for field in required_fields if field not in payload]
 
-        row = rows[0]
-        
-        if (check_password(payload['password_hash'],row[1])):
-            utc_dt_aware = datetime.now(timezone.utc)
+    if missing_fields:
+        response = {'status': StatusCodes['api_error'], 'results': f'Missing fields: {", ".join(missing_fields)}'}
+        return flask.jsonify(response)
+
+    username = payload['username']
+    password = payload['password']
+
+    try:
+        # Consultar a tabela "administrator" para verificar se o usuário é um admin
+        statement = '''
+            SELECT account.id FROM administrator
+            INNER JOIN account ON administrator.account_id = account.id
+            WHERE account.username = %s AND account.password_hash = %s
+        '''
+        values = (username, hash_password(password))
+        cur.execute(statement, values)
+        admin_row = cur.fetchone()
+
+        if admin_row:
+            # O usuário é um admin
+            account_id = admin_row[0]
+
+            utc_dt_aware = datetime.utcnow()
             token = jwt.encode({
-                'user_id': row[2],
+                'user_id': account_id,
+                'role': 'admin',
                 'exp': utc_dt_aware + timedelta(minutes = 120)
             },
-                app.config['SECRET_KEY'])
-            response = {'status': StatusCodes['success'], 'results': f'Inserted user {payload["username"]}'}
+                app.config['SECRET_KEY'], algorithm='HS256')
+            
+            response = {'status': StatusCodes['success'], 'results': f'Logged in as {payload["username"]}'}
             return flask.jsonify({'token': token})
-        else:
-            response = {'status': StatusCodes['api_error'], 'results': f'Incorrect password'}
-            return flask.jsonify(response)
+            
+
+        # Consultar a tabela "artist" para verificar se o usuário é um artista
+        statement = '''
+            SELECT account.id FROM artist
+            INNER JOIN account ON artist.account_id = account.id
+            WHERE account.username = %s AND account.password_hash = %s
+        '''
+        values = (username, hash_password(password))
+        cur.execute(statement, values)
+        artist_row = cur.fetchone()
+
+        if artist_row:
+            # O usuário é um artista
+            account_id = admin_row[0]
+
+            utc_dt_aware = datetime.utcnow()
+            token = jwt.encode({
+                'user_id': account_id,
+                'role': 'artist',
+                'exp': utc_dt_aware + timedelta(minutes = 120)
+            },
+                app.config['SECRET_KEY'], algorithm='HS256')
+            
+            response = {'status': StatusCodes['success'], 'results': f'Logged in as {payload["username"]}'}
+            return flask.jsonify({'token': token})
         
+        # Se o usuário não foi encontrado nas tabelas "administrator" e "artist",
+        # ele é considerado um usuário regular
+        statement = 'SELECT id FROM account WHERE username = %s AND password_hash = %s'
+        values = (username, hash_password(password))
+        cur.execute(statement, values)
+        regular_user_row = cur.fetchone()
+
+        if regular_user_row:
+            # O usuário é um usuário regular
+            account_id = regular_user_row[0]
+              
+            utc_dt_aware = datetime.utcnow()
+            token = jwt.encode({
+                'user_id': account_id,
+                'role': 'artist',
+                'exp': utc_dt_aware + timedelta(minutes = 120)
+            },
+                app.config['SECRET_KEY'], algorithm='HS256')
+            
+            response = {'status': StatusCodes['success'], 'results': f'Logged in as {payload["username"]}'}
+            return flask.jsonify({'token': token})
 
     except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(f'PUT /dbproj/user - error: {error}')
+        logger.error(f'POST /dbproj/login - error: {error}')
         response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
 
     finally:
         if conn is not None:
             conn.close()
-    
-    return flask.jsonify(response)
 
+    return flask.jsonify(response)
 
 @app.route('/dbproj/song', methods=['POST'])
 @token_required
@@ -216,36 +319,60 @@ def add_song(user_id):
 
     logger.debug(f'POST /dbproj/song - payload: {payload}')
 
+    # Verificar se todos os campos obrigatórios estão presentes no payload
+    required_fields = ['song_name', 'release_date', 'publisher', 'other_artists', 'duration', 'ismn', 'genre']
+    missing_fields = [field for field in required_fields if field not in payload]
 
-    if 'song_name' not in payload or 'release_date' not in payload or 'publisher' not in payload:
-        response = {'status': StatusCodes['api_error'], 'results': 'song_name, release_date, or publisher value not in payload'}
+    if missing_fields:
+        response = {'status': StatusCodes['api_error'], 'results': f'Missing fields: {", ".join(missing_fields)}'}
         return flask.jsonify(response)
 
-    if 'other_artists' not in payload:
-        response = {'status': StatusCodes['api_error'], 'results': 'other_artists value not in payload'}
-        return flask.jsonify(response)
+    song_name = payload['song_name']
+    release_date = payload['release_date']
+    publisher_id = payload['publisher']
+    other_artists = payload['other_artists']
+    duration = payload['duration']
+    ismn = payload['ismn']
+    genre = payload['genre']
+    
+    # Get id of current user from token
 
     try:
-        # Insert the song into the song table
-        statement = 'INSERT INTO song (name, release_date, publisher_id) VALUES (%s, %s, %s) RETURNING ismn'
-        values = (payload['song_name'], payload['release_date'], payload['publisher'])
+        # Inserir a música na tabela "song"
+        statement = '''
+            INSERT INTO song (ismn, name, release_date, genre, duration, artist_account_id, publisher_id)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            RETURNING ismn
+        '''
+        values = (ismn, song_name, release_date, genre, duration, user_id, publisher_id)
         cur.execute(statement, values)
-        song_ismn = cur.fetchone()[0]
+        song_id = cur.fetchone()[0]
 
-        # Insert the relationship between the artist and the song into artist_song table
-        for artist_id in payload['other_artists']:
-            statement = 'INSERT INTO artist_song (artist_account_id, song_ismn) VALUES (%s, %s)'
-            values = (artist_id, song_ismn)
+        # Inserir a relação entre o artista atual e a música na tabela "artist_song"
+        statement = '''
+            INSERT INTO artist_song (artist_account_id, song_ismn)
+            VALUES (%s, %s)
+        '''
+        values = (user_id, ismn)
+        cur.execute(statement, values)
+
+        # Inserir as relações entre os outros artistas e a música na tabela "artist_song"
+        for artist_id in other_artists:
+            statement = '''
+                INSERT INTO artist_song (artist_account_id, song_ismn)
+                VALUES (%s, %s)
+            '''
+            values = (artist_id, ismn)
             cur.execute(statement, values)
 
-        # Commit the transaction
+        # Commit a transação
         conn.commit()
-        response = {'status': StatusCodes['success'], 'results': f'Successfully added song with ID: {song_ismn}'}
+        response = {'status': StatusCodes['success'], 'results': f'Inserted song with ID: {song_id}'}
 
     except (Exception, psycopg2.DatabaseError) as error:
         logger.error(f'POST /dbproj/song - error: {error}')
         response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-        # An error occurred, rollback
+        # Ocorreu um erro, faz rollback
         conn.rollback()
 
     finally:
@@ -253,7 +380,6 @@ def add_song(user_id):
             conn.close()
 
     return flask.jsonify(response)
-
 
 @app.route('/dbproj/album', methods=['POST'])
 @token_required
